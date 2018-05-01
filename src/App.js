1703d121a2bdefed179f0e1f13c0a97247b46e48
Thyago F. da Silva <thferreira@gmail.com>
2018-05-01 18:52:08 -0300

    refactor: Abstract BookItem component and other edits

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 // Contrib
 import React from 'react'
 import {Route} from 'react-router-dom'
-// Custom
-// import * as BooksAPI from './BooksAPI'
+// Custom (from fork)
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-import SearchScreen from './components/SearchScreen';
-import IndexScreen from './components/IndexScreen';
+// Custom
+import SearchScreen from './components/SearchScreen'
+import IndexScreen from './components/IndexScreen'
 
 class BooksApp extends React.Component {
-  state = {}
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    // Populate books state piece
+    BooksAPI.getAll().then(books => {
+      this.setState({books})
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <IndexScreen />
+          <IndexScreen books={this.state.books} />
         )}/>
         <Route path='/search' render={() => (
           <SearchScreen />
